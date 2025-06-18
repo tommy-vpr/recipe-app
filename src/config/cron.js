cf592f5f077d9a7cab9aa2d407c471cf76a309c8
tommy-vpr <tommy@cultivatedagency.com>
2025-06-18 12:13:45 -0700

    update cron job to fire ever 14 mins

--- src/config/cron.js
+++ src/config/cron.js
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 
 const KEEP_ALIVE_URL = `${process.env.API_URL}/api/health`;
 
-const keepAliveJob = new CronJob("*/30 * * * * *", async () => {
+const keepAliveJob = new CronJob("*/14 * * * *", async () => {
   try {
     const res = await fetch(KEEP_ALIVE_URL);
     const data = await res.json(); // <-- parse as JSON, not text
