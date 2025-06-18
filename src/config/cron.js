8837e0e0bd8bc894e7029ee37859b7c85a24afe7
tommy-vpr <tommy@cultivatedagency.com>
2025-06-18 12:06:39 -0700

    added api url env

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,10 +1,9 @@
 import { CronJob } from "cron";
-import fetch from "node-fetch"; // Node 18+ can use global fetch
 
-const KEEP_ALIVE_URL = "https://recipe-app-3692.onrender.com/api/health"; // change to your real endpoint
+// Replace with your actual endpoint
+const KEEP_ALIVE_URL = `${process.env.API_URL}/api/health`;
 
-// Cron job to ping every 14 minutes
-// const keepAliveJob = new CronJob("*/14 * * * *", async () => {
+// Cron job to ping every 30 seconds
 const keepAliveJob = new CronJob("*/30 * * * * *", async () => {
   try {
     const res = await fetch(KEEP_ALIVE_URL);
